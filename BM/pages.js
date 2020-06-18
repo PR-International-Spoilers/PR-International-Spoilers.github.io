a3acb571417f284c610263a4783782d42be17a10
JosephcatZ <53020577+JosephcatZ@users.noreply.github.com>
2020-06-18 12:22:55 -0400

    Shall we begin?

---
+++ BM/pages.js
@@ -0,0 +1,9 @@
+ document.write('  <nav>\
+<ul class="pagination"> \
+<li class="page-item enabled"> <a href = "209.html" class="page-link"> 9</a></li>\
+<li class="page-item enabled"> <a href = "210.html" class="page-link"> 10</a></li>\
+<li class="page-item enabled"> <a href = "211.html" class="page-link"> 11</a></li>\
+<li class="page-item enabled"> <a href = "212.html" class="page-link"> 12</a></li>\
+<li class="page-item enabled"> <a href = "222.html" class="page-link"> 22</a></li>\
+</ul>\
+</nav>');
---
+++ navbar.js
@@ -0,0 +1,47 @@
+document.write('<nav class="navbar navbar-expand-lg navbar-light bg-light">\
+  <a class="navbar-brand" href="../index.html">Power Rangers International Spoilers  (V1.1.0)</a>\
+  <button\
+    class="navbar-toggler"\
+    type="button"\
+    data-toggle="collapse"\
+    data-target="#navbarNavAltMarkup"\
+    aria-controls="navbarNavAltMarkup"\
+    aria-expanded="false"\
+    aria-label="Toggle navigation"\
+  >\
+    <span class="navbar-toggler-icon"></span>\
+  </button>\
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
+    <div class="navbar-nav">\
+      <div class="dropdown">\
+        <a\
+          class="nav-link dropdown-toggle"\
+          type="button"\
+          id="navbarDropdown"\
+          role="button"\
+          data-toggle="dropdown"\
+          aria-haspopup="true"\
+          href=""\
+          aria-expanded="false"\
+        >\
+          Beast Morphers Season 2\
+        </a>\
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+          <a class="dropdown-item" href="../BM/209.html" target="_top"\
+            >Episode 9: The Evox Snare</a\
+          >\
+          <a class="dropdown-item" href="../BM/210.html" target="_top"\
+            >Episode 10: Intruder Alert!</a\
+          >\
+          <a class="dropdown-item" href="../BM/211.html" target="_top"\
+            >Episode 11: The Great Good</a\
+          >\
+          <a class="dropdown-item" href="../BM/212.html" target="_top"\
+            >Episode 12: Finders Keepers</a\
+          >\
+        </div>\
+      </div>\
+    </div>\
+  </div>\
+</nav> \
+');
